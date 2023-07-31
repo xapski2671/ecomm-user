9d6de0ec560858ddefd128a2718e67374afef66e
xapski <xapski2671@gmail.com>
2023-07-31 17:21:12 +0100

    cm-00

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import Head from "next/head"
+import { createGlobalStyle } from "styled-components"
+
+const GLobalStyles = createGlobalStyle`
+body {
+  padding: 0;
+  margin: 0;
+}
+`
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<GLobalStyles />
+			<Component {...pageProps} />
+		</>
+	)
 }
